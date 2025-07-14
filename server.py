89686a2328239c35174c70cd4d07b5bac7596e78
SunSet02 <87053637+SunSet02@users.noreply.github.com>
2025-07-14 11:27:56 +0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,12 @@
+# server.py
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Demo 🚀")
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+if __name__ == "__main__":
+    mcp.run()
